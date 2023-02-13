95f2854d3b3b144ecf82daafc59879443552be72
swapnil1305 <swapnil.satpute132@gmail.com>
2023-02-14 01:20:12 +0530

    4

--- controller/user.js
+++ controller/user.js
@@ -21,3 +21,24 @@ exports.signup = async(req, res, next) => {
         res.status(500).json(err);
     }
 }
+
+exports.login = async(req, res, next) => {
+    try{
+        const{email, password} = req.body;
+        if (isstringinvalid(email) || isstringinvalid(password)){
+            return res.status(400).json({message:"Email or Password is missing!!"})
+        }
+        const user = await User.findAll({ where: {email}})
+        if(user.length > 0){
+            if(user[0].password === password){
+                res.status(200).json({success: true, message: "User logged in successfully"})
+            } else{
+                return res.status(400).json({success: false, message: "Password is incorrect"})
+            }
+        }else{
+            return res.status(404).json({success: false, message: "User does not exist"})
+        }
+    }catch(err){
+        res.status(500).json({message: err, success: false})
+    }
+}
--- login.js
+++ login.js
@@ -1,17 +1,17 @@
 async function login(e){
     try{
     e.preventDefault();
-    console.log(e.target.name);
+    console.log(e.target.name.value);
 
     const loginDetails = {
-        email: e.email.target.value,
-        password: e.password.target.value
+        email: e.target.email.value,
+        password: e.target.password.value
     }
-    console.log(loginDetails)
+    console.log(loginDetails);
     const response = await axios.post('http://localhost:4000/user/login', loginDetails)
             alert(response.data.message)
 }catch(err){
-        console.log(JSON.stringify(err))
-        document.body.innerHTML += `<div style="color:red">${err.message}<div>` ;
+        //console.log(JSON.stringify(err))
+        document.body.innerHTML += `<div style="color:red;">${err.message}<div>` ;
     }
 }
