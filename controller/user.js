498edfe6b9a0527e12f5a3f753910eaf71a59c25
swapnil1305 <swapnil.satpute132@gmail.com>
2023-02-14 12:55:56 +0530

    5

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 function isstringinvalid(string){
     if(string == undefined || string.length === 0){
@@ -15,8 +16,12 @@ exports.signup = async(req, res, next) => {
         if (isstringinvalid(name) || isstringinvalid(email) || isstringinvalid(password)){
             return res.status(400).json({err: "something is missing!!"})
         }
-        await User.create({name, email, password})
+        const saltrounds = 10;
+        bcrypt.hash(password, saltrounds, async (err, hash) => {
+            console.log(err);
+            await User.create({name, email, password:hash})
         res.status(201).json({message: "successfully created new user"})
+        })
     }catch(err) {
         res.status(500).json(err);
     }
@@ -30,14 +35,20 @@ exports.login = async(req, res, next) => {
         }
         const user = await User.findAll({ where: {email}})
         if(user.length > 0){
-            if(user[0].password === password){
-                res.status(200).json({success: true, message: "User logged in successfully"})
-            } else{
-                return res.status(400).json({success: false, message: "Password is incorrect"})
-            }
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if(err){
+                    throw new Error('Something went wrong')
+                }
+                if (result === true){
+                    res.status(200).json({success: true, message: "User logged in successfully"})   
+                }
+                else{
+                    return res.status(400).json({success: false, message: "Password is incorrect"})
+                }
+            })
         }else{
             return res.status(404).json({success: false, message: "User does not exist"})
-        }
+        } 
     }catch(err){
         res.status(500).json({message: err, success: false})
     }
