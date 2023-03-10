c5990f6fba1d47ec7a4cd0412384d0aed2ea09a1
swapnil1305 <swapnil.satpute132@gmail.com>
2023-03-10 21:31:55 +0530

    private keys moved

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 const path = require('path')
+require('dotenv').config()
+//const fs = require('fs')
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const sequelize = require('./util/database');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
+//models--->>
 const User = require('./models/user');
 const Expense = require('./models/expense');
 const Order = require('./models/order')
 const Forgotpassword = require('./models/forgotpassword')
 const DownloadedFile=require('./models/downloadedFile');
 
+//routers--->>
 const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense')
 const purchaseRoutes = require('./routes/purchase');
 const resetPasswordRoutes = require('./routes/resetpassword')
 const premiumFeatureRoutes = require('./routes/premiumFeature');
+const downloadFilesRoute= require('./routes/allDownload')
 
 const app = express();
-const dotenv = require('dotenv');
 
-dotenv.config();
+//const dotenv = require('dotenv');
+//dotenv.config();
 
+//const accessLogStream = fs.createReadStream(path.join(__dirname, 'access.log'), {flags: 'a'})
 app.use(cors());
-
 app.use(bodyParser.json()); //for handling forms
 app.use(express.json());  //for handling jsons
+app.use(helmet());
+app.use(compression());
+//app.use(morgan('combined', {stream: accessLogStream}));
 
+//middlewares--->>
 app.use('/user', userRoutes);
 app.use('/expense', expenseRoutes);
 app.use('/purchase', purchaseRoutes);
 app.use('/premium', premiumFeatureRoutes);
 app.use('/password', resetPasswordRoutes);
+app.use('/download', downloadFilesRoute);
 
 
 User.hasMany(Expense);
--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize ('expenseproject', 'root', 'swappnil@7', {
+const sequelize = new Sequelize (process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.DB_HOST,
 })
 module.exports = sequelize;
